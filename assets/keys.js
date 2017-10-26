85fb95fa16aded6fb922b168515ce647ff47594f
Michelle Ocampo <manneocampo@gmail.com>
2017-10-25 22:40:49 -0400

    added the starter files

---
+++ assets/keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '923336829654495232-vVmxKjAxHCcD2IgytibR3ux1VRzGWyb',
+  access_token_secret: 'unKjIGuUaVv5EUHgxNxcJJrBkTA380CXSSQjSBNmCCVyr',
+}
+
+module.exports = twitterKeys;
