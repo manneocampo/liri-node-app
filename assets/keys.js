b0b477177b2636411d41304906ba201f037fc691
Michelle Ocampo <manneocampo@gmail.com>
2017-10-29 16:10:59 -0400

    added spotify code -done

--- assets/keys.js
+++ assets/keys.js
@@ -1,10 +1,17 @@
 console.log('this is loaded');
 
-var twitterKeys = {
-  consumer_key: 'AR5W5pt6A5MKOlq2GpMCxHT9O',
-  consumer_secret: '44PCmiG55cY9ix4mTFTyKzvFi43lYvFAZqwpc4O6k4rkGcoNRA',
-  access_token_key: '923336829654495232-vVmxKjAxHCcD2IgytibR3ux1VRzGWyb',
-  access_token_secret: 'unKjIGuUaVv5EUHgxNxcJJrBkTA380CXSSQjSBNmCCVyr',
-}
+var keys = {
+	twitterKeys: {
+  		consumer_key: 'AR5W5pt6A5MKOlq2GpMCxHT9O',
+  		consumer_secret: '44PCmiG55cY9ix4mTFTyKzvFi43lYvFAZqwpc4O6k4rkGcoNRA',
+  		access_token_key: '923336829654495232-vVmxKjAxHCcD2IgytibR3ux1VRzGWyb',
+  		access_token_secret: 'unKjIGuUaVv5EUHgxNxcJJrBkTA380CXSSQjSBNmCCVyr',
+	},
+	spotifyKeys: {
+		id: "4ee2b10a6fa14d9fbb7381a7d8a3de57",
+	  	secret: "1591902d531843629a67e43a56e6693c",
+	},
+};
 
-module.exports = twitterKeys;
+
+module.exports = keys;
--- assets/liri.js
+++ assets/liri.js
@@ -1,6 +1,7 @@
 var keys = require("./keys.js");
 var Twitter = require("twitter");
 var args = process.argv;
+var Spotify = require("node-spotify-api");
 
 switch (args[2]) {
 	case "my-tweets":
@@ -19,7 +20,7 @@ switch (args[2]) {
 
 function getTweets(){
 	console.log("twitter request", keys);
-	var client = new Twitter(keys);
+	var client = new Twitter(keys.twitterKeys);
 	 
 	var params = {screen_name: 'coding_cactus1'};
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -33,11 +34,26 @@ function getTweets(){
 }
 
 function spotify() {
+	var search = process.argv[3] || "The Sign by Ace of Base";
 	console.log("spotify request");
+	var spotify = new Spotify(keys.spotifyKeys);
+	 
+	spotify.search({ type: 'track', query: search }, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+	  for (var i=0; i<data.tracks.items.length; i++) {
+	  	console.log("Album:       ", data.tracks.items[i].album.name); 
+		console.log("Artist:      ", data.tracks.items[i].artists[0].name);
+		console.log("Preview url: ", data.tracks.items[i].preview_url);
+		console.log("Song name:   ", data.tracks.items[i].name + "\n");
+	  }
+	});
 }
 
 function ombd(){
 	console.log("movie request");
+
 }
 
 function bossy(){
