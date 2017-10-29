d2f277c96cba3ad8db6e5bbc53ab7379fe7faf31
Michelle Ocampo <manneocampo@gmail.com>
2017-10-29 19:26:47 -0400

    finished final part- all done

--- assets/liri.js
+++ assets/liri.js
@@ -3,24 +3,28 @@ var Twitter = require("twitter");
 var args = process.argv;
 var Spotify = require("node-spotify-api");
 var request = require("request");
+var fs = require("fs");
 
-switch (args[2]) {
-	case "my-tweets":
-		getTweets();
-		break;
-	case "spotify-this-song":
-		spotify();
-		break;
-	case "movie-this":
-		omdb();
-		break;
-	case "do-what-it-says":
-		bossy();
-		break; 
+function choose(command, value) {
+		switch (command) {
+		case "my-tweets":
+			getTweets();
+			break;
+		case "spotify-this-song":
+			spotify(value);
+			break;
+		case "movie-this":
+			omdb(value);
+			break;
+		case "do-what-it-says":
+			bossy();
+			break; 
+	}
 }
 
+
 function getTweets(){
-	console.log("twitter request", keys);
+	// console.log("twitter request", keys);
 	var client = new Twitter(keys.twitterKeys);
 	 
 	var params = {screen_name: 'coding_cactus1'};
@@ -34,9 +38,9 @@ function getTweets(){
 	});
 }
 
-function spotify() {
-	var search = process.argv[3] || "The Sign by Ace of Base";
-	console.log("spotify request");
+function spotify(value) {
+	var search = value || "The Sign by Ace of Base";
+	// console.log("spotify request");
 	var spotify = new Spotify(keys.spotifyKeys);
 	 
 	spotify.search({ type: 'track', query: search }, function(err, data) {
@@ -52,8 +56,8 @@ function spotify() {
 	});
 }
 
-function omdb(){
-	var search = process.argv[3];
+function omdb(value){
+	var search = value || "Mr. Nobody";
 	var url = "http://www.omdbapi.com/?apikey=" + keys.omdbKey + "&type=movie&t=" + search 
 	request(url, function (error, response, body) {
 	  if (error){
@@ -61,7 +65,7 @@ function omdb(){
 	  }   
 
  	var body = JSON.parse(body);
-	  console.log('body:', body); // Print the HTML for the Google homepage.
+	  // console.log('body:', body); // Print the HTML for the Google homepage.
 	  console.log("Title:", body.Title);
 	  console.log("Year:", body.Year);
 	  console.log("IMDB Rating:", body.imdbRating);
@@ -69,15 +73,23 @@ function omdb(){
 	  console.log("Country:", body.Country);
 	  console.log("Language:", body.Language);
 	  console.log("Plot:", body.Plot);
-	  console.log("Actors:", body.Actors);
+	  console.log(`Actors: ${body.Actors}`);
 
 	});
-	console.log("movie request");
+	// console.log("movie request");
 
 }
 
 function bossy(){
-	console.log("bossy request");
-}
+	fs.readFile("random.txt", "utf8", function(err, data) {
+	    if (err) {
+	      return console.log(err);
+	    }
+	    var dataArray= data.split(',');
+	    choose(dataArray[0], dataArray[1]);
+	});
+	// console.log("bossy request");
 
+}
+choose(process.argv[2],process.argv[3]);
 
